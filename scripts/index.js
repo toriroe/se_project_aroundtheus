dd9ca2fcd51fb29c5110ebe0c22fcfd007a324fe
Tori <toriroettger@gmail.com>
2023-03-23 17:49:49 -0500

    project 4 stage 1

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let initialCards = [
+  (firstPhoto = {
+    name: "Alaverdi, Armenia",
+    link: "https://unsplash.com/photos/RkOtjbPuHZw",
+  }),
+  (secondPhoto = {
+    name: "Angkor Wat, Cambodia",
+    link: "https://unsplash.com/photos/4KKVELjJsNw",
+  }),
+  (thirdPhoto = {
+    name: "Arches National Park, USA",
+    link: "https://unsplash.com/photos/XuQGqCBpNGk",
+  }),
+  (fourthPhoto = {
+    name: "Redwood National Park, USA",
+    link: "https://unsplash.com/photos/V0OUeuUAtz4",
+  }),
+  (fifthPhoto = {
+    name: "Mount Fuji, Japan",
+    link: "https://unsplash.com/photos/9Qwbfa_RM94",
+  }),
+  (sixthPhoto = {
+    name: "Deadvlei, Namibia",
+    link: "https://unsplash.com/photos/jTJ9-4ESzU4",
+  }),
+];
