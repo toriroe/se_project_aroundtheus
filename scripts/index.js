4dd739384e118439234f621d78da06717a6205cb
Tori <toriroettger@gmail.com>
2023-04-01 17:13:36 -0500

    first attempt submitting project 4 stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,38 @@
 let initialCards = [
-  (firstPhoto = {
+  {
     name: "Alaverdi, Armenia",
     link: "https://unsplash.com/photos/RkOtjbPuHZw",
-  }),
-  (secondPhoto = {
+  },
+  {
     name: "Angkor Wat, Cambodia",
     link: "https://unsplash.com/photos/4KKVELjJsNw",
-  }),
-  (thirdPhoto = {
+  },
+  {
     name: "Arches National Park, USA",
     link: "https://unsplash.com/photos/XuQGqCBpNGk",
-  }),
-  (fourthPhoto = {
+  },
+  {
     name: "Redwood National Park, USA",
     link: "https://unsplash.com/photos/V0OUeuUAtz4",
-  }),
-  (fifthPhoto = {
+  },
+  {
     name: "Mount Fuji, Japan",
     link: "https://unsplash.com/photos/9Qwbfa_RM94",
-  }),
-  (sixthPhoto = {
+  },
+  {
     name: "Deadvlei, Namibia",
     link: "https://unsplash.com/photos/jTJ9-4ESzU4",
-  }),
+  },
 ];
 
 let profileEditButton = document.querySelector("#profile-edit-button");
+let profileEditModal = document.querySelector("#profile-edit-modal");
+let formExitButton = document.querySelector("#form-exit-button");
 
-console.log(profileEditButton);
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+formExitButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
