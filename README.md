96788fcc202ded6ed821b07fb83b44d6e0fd4f99
Tori <toriroettger@gmail.com>
2023-04-03 20:29:11 -0500

    first try submitting project 4 final stage

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Practicum Around the U.S. Project
+# Practicum Around the U.S. Project (Part 2)
 
-This is the third project of the Web Development program at Practicum. It was created using HTML and CSS, based on the Figma design layout. The web page is responsive, so it has been designed for both desktop and mobile devices. Photos have been optimized using "TinyPng" (https://tinypng.com/).
+This is the fourth project of the Web Development program at Practicum - it is an updated version of the third project which includes previous features as well as a modal box for editing profile info, and cards rendered through Javascript rather than static HTML code. It was created using HTML, CSS,and Javascript based on the Figma design layout. The web page is responsive, so it has been designed for both desktop and mobile devices. Photos have been optimized using "TinyPng" (https://tinypng.com/).
 
 - [Link to the project design on Figma](https://www.figma.com/file/ii4xxsJ0ghevUOcssTlHZv/Sprint-3%3A-Around-the-US?node-id=0%3A1)
 
@@ -12,10 +12,12 @@ This is the third project of the Web Development program at Practicum. It was cr
 - Use of media queries so that all elements are displayed properly on different screen resolutions
 - Flat BEM
 - Use of psuedo-classes to customize hover states for buttons
+- Use of Javascript to implement limited interactivity to profile edit modal box (change name and description of profile)
+- Card elements and images rendered through Javascript using <template> element
 
 ## Plan on improving the project
 
-- Implemeting interactivity to the site, so users can add and remove photos, like photos of other users, and make a few minor adjustments to their own profile.
+- Adding increased interactivity, so that users can "like" images, connect site to a server so that changes made to profile and cards are saved upon page refresh.
 
 # GitHub Page
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,77 @@
+//This section refers to the cards content
+
 let initialCards = [
   {
     name: "Alaverdi, Armenia",
-    link: "https://unsplash.com/photos/RkOtjbPuHZw",
+    link: "https://images.unsplash.com/photo-1543862475-eb136770ae9b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
   },
   {
     name: "Angkor Wat, Cambodia",
-    link: "https://unsplash.com/photos/4KKVELjJsNw",
+    link: "https://images.unsplash.com/photo-1600807455249-89d2e285b9e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
   },
   {
     name: "Arches National Park, USA",
-    link: "https://unsplash.com/photos/XuQGqCBpNGk",
+    link: "https://images.unsplash.com/photo-1527333656061-ca7adf608ae1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
   },
   {
     name: "Redwood National Park, USA",
-    link: "https://unsplash.com/photos/V0OUeuUAtz4",
+    link: "https://images.unsplash.com/photo-1547014762-3a94fb4df70a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80",
   },
   {
     name: "Mount Fuji, Japan",
-    link: "https://unsplash.com/photos/9Qwbfa_RM94",
+    link: "https://images.unsplash.com/photo-1490806843957-31f4c9a91c65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
   },
   {
     name: "Deadvlei, Namibia",
-    link: "https://unsplash.com/photos/jTJ9-4ESzU4",
+    link: "https://images.unsplash.com/photo-1448831338187-78296e6fdc4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80",
   },
 ];
+
+let cardTemplate = document.querySelector("#card-template").content;
+let cardList = document.querySelector(".gallery__list");
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  cardTitle.textContent = cardData.name;
+  cardImage.alt = "Photo of " + cardTitle.textContent;
+  cardImage.src = cardData.link;
+
+  return cardElement;
+}
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardList.append(cardElement);
+});
+
+//This section refers to the profile edit modal
 
 let profileEditButton = document.querySelector("#profile-edit-button");
 let profileEditModal = document.querySelector("#profile-edit-modal");
 let formExitButton = document.querySelector("#form-exit-button");
+let inputName = document.querySelector("#form-input-name");
+let inputDescription = document.querySelector("#form-input-description");
+let profileName = document.querySelector(".profile__name");
+let profileSubtitle = document.querySelector(".profile__subtitle");
+let formSubmitButton = document.querySelector("#form-submit-button");
 
-profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add("modal_opened");
-});
+function toggleForm() {
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileSubtitle.textContent;
+  profileEditModal.classList.toggle("modal_opened");
+}
 
-formExitButton.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
-});
+profileEditButton.addEventListener("click", toggleForm);
+
+formExitButton.addEventListener("click", toggleForm);
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileSubtitle.textContent = inputDescription.value;
+  profileEditModal.classList.toggle("modal_opened");
+}
+
+formSubmitButton.addEventListener("click", handleProfileFormSubmit);
