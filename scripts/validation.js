b21d00afbbb636a2effb77de590f93e2f8623678
Tori <toriroettger@gmail.com>
2023-05-09 22:16:44 -0500

    fixed form submit button classes (BEM)

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,7 +1,7 @@
 const config = {
   formSelector: ".modal__form",
   inputSelector: ".form__input",
-  submitButtonSelector: ".form__button-submit",
+  submitButtonSelector: ".form__button",
   inactiveButtonClass: "form__button_disabled",
   inputErrorClass: "form__input_type_error",
   errorClass: "form__error_visible",
