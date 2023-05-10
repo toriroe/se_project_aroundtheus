8d36ea013b415051e84f38861814fe02706b449d
Tori <toriroettger@gmail.com>
2023-05-09 22:08:46 -0500

    first attempt submitting project 6

--- scripts/index.js
+++ scripts/index.js
@@ -100,10 +100,29 @@ initialCards.forEach((cardData) => renderCard(cardData, cardList));
 
 function openModal(modal) {
   modal.classList.add("modal_opened");
+  modal.addEventListener("mousedown", closeModalOutsideClick);
+  document.addEventListener("keydown", closeModalEsc);
 }
 
 function closeModal(modal) {
   modal.classList.remove("modal_opened");
+  modal.removeEventListener("mousedown", closeModalOutsideClick);
+  document.removeEventListener("keydown", closeModalEsc);
+}
+
+// CLOSE MODALS WITH ESC/CLICKING OUTSIDE MODAL BOX
+
+function closeModalOutsideClick(evt) {
+  if (evt.target === evt.currentTarget) {
+    closeModal(evt.currentTarget);
+  }
+}
+
+function closeModalEsc(evt) {
+  if (evt.key === "Escape") {
+    const currentModal = document.querySelector(".modal_opened");
+    closeModal(currentModal);
+  }
 }
 
 // OPEN AND FILL PROFILE EDIT MODAL
